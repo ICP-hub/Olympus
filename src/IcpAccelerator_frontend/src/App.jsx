c342f213c1e546d1e92199d7c9b0b8c4023fcbf6
Nafeesahmed786 <nafeesahmed01.na@gmail.com>
2024-02-19 13:24:10 +0530

    added app.jsx

--- src/IcpAccelerator_frontend/src/App.jsx
+++ src/IcpAccelerator_frontend/src/App.jsx
@@ -73,7 +73,7 @@ const App = () => {
       {/* <ProjectDetails/> */}
       {/* <Home/> */}
       {/* <UserProfile/> */}
-      <RoleSelector />
+      {/* <RoleSelector /> */}
       <AppRoutes/>
     </>
   );
