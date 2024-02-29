b3e933fb94366994ece46cb55911263db3322c4e
vipulquadb <vipul.taneja@quadbtech.com>
2024-02-29 11:24:18 +0530

    roadmap

--- src/IcpAccelerator_frontend/src/components/Utils/Data/SvgData.jsx
+++ src/IcpAccelerator_frontend/src/components/Utils/Data/SvgData.jsx
@@ -449,3 +449,10 @@ export const  twitter = (
   
 
 );
+export  const time= (
+  <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="11" cy="11" r="10" stroke="#737373" stroke-width="1.5"/>
+<path d="M8.5 8.5L11.9999 11.9996M15 7L10 12" stroke="#737373" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+);
