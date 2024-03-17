41392c4bf28f20ed461e3d4ce0020ad49168f23e
vinayakkalra <vinayak.kalra@quadbtech.com>
2024-03-17 23:37:46 +0530

    changes in script

--- tests/test_data.sh
+++ tests/test_data.sh
@@ -1,58 +1,58 @@
-# . ./local_test_data/test_user_data.sh
-# . ./local_test_data/test_project_data.sh
-# . ./local_test_data/test_investor.sh
-# . ./local_test_data/test_mentor_data.sh
-# . ./local_test_data/test_jobs.sh
-# . ./local_test_data/test_team.sh
+. ./local_test_data/test_user_data.sh
+. ./local_test_data/test_project_data.sh
+. ./local_test_data/test_investor.sh
+. ./local_test_data/test_mentor_data.sh
+. ./local_test_data/test_jobs.sh
+. ./local_test_data/test_team.sh
 
-dfx identity remove user1
-dfx identity remove user2
-dfx identity remove user3
-dfx identity remove user4
-dfx identity remove user5
-dfx identity remove user6
-dfx identity remove user7
-dfx identity remove user8
-dfx identity remove user9
-dfx identity remove user10
-dfx identity remove user11
-dfx identity remove user12
-dfx identity remove user13
-dfx identity remove user14
-dfx identity remove user15
-dfx identity remove user16
-dfx identity remove user17
-dfx identity remove user18
-dfx identity remove user19
-dfx identity remove user20
-dfx identity remove user21
-dfx identity remove user22
-dfx identity remove user23
-dfx identity remove user24
-dfx identity remove user25
+# dfx identity remove user1
+# dfx identity remove user2
+# dfx identity remove user3
+# dfx identity remove user4
+# dfx identity remove user5
+# dfx identity remove user6
+# dfx identity remove user7
+# dfx identity remove user8
+# dfx identity remove user9
+# dfx identity remove user10
+# dfx identity remove user11
+# dfx identity remove user12
+# dfx identity remove user13
+# dfx identity remove user14
+# dfx identity remove user15
+# dfx identity remove user16
+# dfx identity remove user17
+# dfx identity remove user18
+# dfx identity remove user19
+# dfx identity remove user20
+# dfx identity remove user21
+# dfx identity remove user22
+# dfx identity remove user23
+# dfx identity remove user24
+# dfx identity remove user25
 
-dfx identity new user1 --disable-encryption || true
-dfx identity new user2 --disable-encryption || true
-dfx identity new user3 --disable-encryption || true
-dfx identity new user4 --disable-encryption || true
-dfx identity new user5 --disable-encryption || true
-dfx identity new user6 --disable-encryption || true
-dfx identity new user7 --disable-encryption || true
-dfx identity new user8 --disable-encryption || true
-dfx identity new user9 --disable-encryption || true
-dfx identity new user10 --disable-encryption || true
-dfx identity new user11 --disable-encryption || true
-dfx identity new user12 --disable-encryption || true
-dfx identity new user13 --disable-encryption || true
-dfx identity new user14 --disable-encryption || true
-dfx identity new user15 --disable-encryption || true
-dfx identity new user16 --disable-encryption || true
-dfx identity new user17 --disable-encryption || true
-dfx identity new user18 --disable-encryption || true
-dfx identity new user19 --disable-encryption || true
-dfx identity new user20 --disable-encryption || true
-dfx identity new user21 --disable-encryption || true
-dfx identity new user22 --disable-encryption || true
-dfx identity new user23 --disable-encryption || true
-dfx identity new user24 --disable-encryption || true
-dfx identity new user25 --disable-encryption || true
+# dfx identity new user1 --disable-encryption || true
+# dfx identity new user2 --disable-encryption || true
+# dfx identity new user3 --disable-encryption || true
+# dfx identity new user4 --disable-encryption || true
+# dfx identity new user5 --disable-encryption || true
+# dfx identity new user6 --disable-encryption || true
+# dfx identity new user7 --disable-encryption || true
+# dfx identity new user8 --disable-encryption || true
+# dfx identity new user9 --disable-encryption || true
+# dfx identity new user10 --disable-encryption || true
+# dfx identity new user11 --disable-encryption || true
+# dfx identity new user12 --disable-encryption || true
+# dfx identity new user13 --disable-encryption || true
+# dfx identity new user14 --disable-encryption || true
+# dfx identity new user15 --disable-encryption || true
+# dfx identity new user16 --disable-encryption || true
+# dfx identity new user17 --disable-encryption || true
+# dfx identity new user18 --disable-encryption || true
+# dfx identity new user19 --disable-encryption || true
+# dfx identity new user20 --disable-encryption || true
+# dfx identity new user21 --disable-encryption || true
+# dfx identity new user22 --disable-encryption || true
+# dfx identity new user23 --disable-encryption || true
+# dfx identity new user24 --disable-encryption || true
+# dfx identity new user25 --disable-encryption || true
