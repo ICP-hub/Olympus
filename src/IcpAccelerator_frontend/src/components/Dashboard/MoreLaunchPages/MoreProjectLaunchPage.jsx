959e25c6cca451d10bca0ed5f378dc753726d3e7
abhishekpandit5599 <abhishekpandit5599@gmail.com>
2024-03-18 11:46:42 +0530

    changes

---
+++ src/IcpAccelerator_frontend/src/components/Dashboard/MoreLaunchPages/MoreProjectLaunchPage.jsx
@@ -0,0 +1,276 @@
+import React, { useState, useEffect, useRef } from "react";
+import ment from "../../../../assets/images/ment.jpg";
+import girl from "../../../../assets/images/girl.jpeg";
+import hover from "../../../../assets/images/hover.png";
+import { IcpAccelerator_backend } from "../../../../../declarations/IcpAccelerator_backend/index";
+import uint8ArrayToBase64 from "../../Utils/uint8ArrayToBase64";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const MoreProjectLaunchPage = () => {
+  const actor = useSelector((currState) => currState.actors.actor);
+  const isAuthenticated = useSelector((curr) => curr.internet.isAuthenticated);
+
+  const [isHovered, setIsHovered] = useState(false);
+  const [percent, setPercent] = useState(0);
+  const [showLine, setShowLine] = useState({});
+  const tm = useRef(null);
+  const navigate = useNavigate();
+  // Gradient color stops, changes when hovered
+  const gradientStops = isHovered
+    ? { stop1: "#4087BF", stop2: "#3C04BA" }
+    : { stop1: "#B5B5B5", stop2: "#5B5B5B" };
+
+
+  const handleClickPlusOne = (id) => {
+    setShowLine((prevShowLine) => ({
+      ...prevShowLine,
+      [id]: !prevShowLine[id],
+    }));
+  };
+
+  const defaultArray = [
+    {
+      projectName: "Awesome Project",
+      projectId: "ABC123",
+      projectImage: ment,
+      userImage: girl,
+      principalId: "user123",
+      projectDescription: "This is an amazing project focused on innovation.",
+      projectAreaOfFocus: "Technology",
+    },
+    {
+      projectName: "Creative Initiative",
+      projectId: "DEF456",
+      projectImage: ment,
+      userImage: girl,
+      principalId: "user456",
+      projectDescription:
+        "A creative initiative aiming to bring positive change.",
+      projectAreaOfFocus: "Art and Design",
+    },
+    {
+      projectName: "Community Outreach",
+      projectId: "GHI789",
+      projectImage: ment,
+      userImage: girl,
+      principalId: "user789",
+      projectDescription:
+        "Engaging community outreach program fostering collaboration.",
+      projectAreaOfFocus: "Social Impact",
+    },
+    {
+      projectName: "Community Outreach",
+      projectId: "GHI789",
+      projectImage: ment,
+      userImage: girl,
+      principalId: "user789",
+      projectDescription:
+        "Engaging community outreach program fostering collaboration.",
+      projectAreaOfFocus: "Social Impact",
+    },
+    {
+      projectName: "Community Outreach",
+      projectId: "GHI789",
+      projectImage: ment,
+      userImage: girl,
+      principalId: "user789",
+      projectDescription:
+        "Engaging community outreach program fostering collaboration.",
+      projectAreaOfFocus: "Social Impact",
+    },
+    {
+      projectName: "Community Outreach",
+      projectId: "GHI789",
+      projectImage: ment,
+      userImage: girl,
+      principalId: "user789",
+      projectDescription:
+        "Engaging community outreach program fostering collaboration.",
+      projectAreaOfFocus: "Social Impact",
+    },
+    {
+      projectName: "Community Outreach",
+      projectId: "GHI789",
+      projectImage: ment,
+      userImage: girl,
+      principalId: "user789",
+      projectDescription:
+        "Engaging community outreach program fostering collaboration.",
+      projectAreaOfFocus: "Social Impact",
+    },
+  ];
+
+  const [noData, setNoData] = useState(null);
+  const [allProjectData, setAllProjectData] = useState([]);
+
+  const getAllProject = async (caller) => {
+    await caller
+      .list_all_projects()
+      .then((result) => {
+        console.log("result-in-get-all-projects", result);
+
+        if (!result || result.length == 0) {
+          setNoData(true);
+          setAllProjectData(defaultArray);
+        } else {
+          setAllProjectData(result);
+          setNoData(false);
+        }
+      })
+      .catch((error) => {
+        setNoData(true);
+        setAllProjectData(defaultArray);
+        console.log("error-in-get-all-projects", error);
+      });
+  };
+
+  useEffect(() => {
+    setAllProjectData(defaultArray);
+    if (actor) {
+      getAllProject(actor);
+    } else {
+      getAllProject(IcpAccelerator_backend);
+    }
+  }, [actor]);
+
+  const handleNavigate = (projectId) => {
+    if (isAuthenticated) {
+      navigate(`/individual-project-details/${projectId}`);
+    }
+  };
+
+  return (
+    <div className="" style={{minHeight: "60vh"}}>
+      <div className="flex-wrap flex flex-row">
+        {allProjectData &&
+          allProjectData.map((data, index) => {
+            let projectName = "";
+            let projectId = "";
+            let projectImage = "";
+            let userImage = "";
+            let principalId = "";
+            let projectDescription = "";
+            let projectAreaOfFocus = "";
+            if (noData === false) {
+              projectName = data[1]?.project_profile[0]?.params?.project_name;
+              projectId = data[1]?.project_profile[0]?.uid;
+              projectImage = uint8ArrayToBase64(
+                data[1]?.project_profile[0]?.params?.project_logo
+              );
+              userImage = uint8ArrayToBase64(
+                data[1]?.project_profile[0]?.params?.user_data
+                  ?.profile_picture[0]
+              );
+              principalId = data[0].toText();
+              projectDescription =
+                data[1]?.project_profile[0]?.params?.project_description;
+              projectAreaOfFocus =
+                data[1]?.project_profile[0]?.params?.project_area_of_focus;
+            } else {
+              projectName = data.projectName;
+              projectId = data.projectId;
+              projectImage = data.projectImage;
+              userImage = data.userImage;
+              principalId = data.principalId;
+              projectDescription = data.projectDescription;
+              projectAreaOfFocus = data.projectAreaOfFocus;
+            }
+            return (
+              <div className="w-full sm:w-1/2 md:w-1/3 mb-2 px-3" key={index}>
+                <div className="justify-between items-baseline mb-4 flex-wrap bg-white m-2 overflow-hidden rounded-lg shadow-lg">
+                  <div className="p-4">
+                    <div className="flex justify-between items-baseline mb-4 flex-wrap w-[265px]">
+                      <div className="flex items-baseline w-1/2">
+                        <img
+                          className="rounded-full w-12 h-12 object-cover"
+                          src={projectImage}
+                          alt="profile"
+                        />
+                        <h1 className="font-bold text-nowrap truncate w-[220px]">
+                          {projectName}
+                        </h1>
+                      </div>
+                      <div className="flex items-baseline w-1/2">
+                        <img
+                          className="h-5 w-5 rounded-full mr-2"
+                          src={userImage}
+                          alt="not found"
+                        />
+                        <p className="text-xs truncate w-20">{principalId}</p>
+                      </div>
+                    </div>
+                    <div className="mb-4 flex items-baseline">
+                      <svg
+                        width="100%"
+                        height="8"
+                        className="rounded-lg"
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                      >
+                        <defs>
+                          <linearGradient
+                            id={`gradient-${projectId}`}
+                            x1="0%"
+                            y1="0%"
+                            x2="100%"
+                            y2="0%"
+                          >
+                            <stop
+                              offset="0%"
+                              stopColor={gradientStops.stop1}
+                              stopOpacity="1"
+                            />
+                            <stop
+                              offset="100%"
+                              stopColor={gradientStops.stop2}
+                              stopOpacity="1"
+                            />
+                          </linearGradient>
+                        </defs>
+                        <rect
+                          x="0"
+                          y="0"
+                          width={`${percent}%`}
+                          height="10"
+                          fill={`url(#gradient-${projectId})`}
+                        />
+                      </svg>
+                      <div className="ml-2 text-nowrap text-sm">Level 2</div>
+                    </div>
+                    <p className="text-gray-700 text-sm md:line-clamp-8 sxs:line-clamp-4 sm:line-clamp-6 line-clamp-8 h-36">
+                      {projectDescription}
+                    </p>
+                    {projectAreaOfFocus ?
+                      <div className="flex gap-2 mt-2 text-xs">
+                        <p>{projectAreaOfFocus}</p>
+
+                        <p
+                          onClick={() =>
+                            handleNavigate(projectId)
+                          }
+                          className="cursor-pointer"
+                        >
+                          +1 more
+                        </p>
+                      </div> : ''}
+
+                    <button
+                      className="mt-4 bg-transparent text-black px-4 py-1 rounded uppercase w-full text-center border border-gray-300 font-bold hover:bg-[#3505B2] hover:text-white transition-colors duration-200 ease-in-out"
+                      onClick={() =>
+                        handleNavigate(projectId)
+                      }
+                    >
+                      KNOW MORE
+                    </button>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default MoreProjectLaunchPage;
---
+++ src/IcpAccelerator_frontend/src/components/Dashboard/MoreLaunchPages/ViewInvestors.jsx
@@ -0,0 +1,172 @@
+import React, { useEffect, useState } from "react";
+import image from "../../../../assets/images/samya.jpg";
+import uint8ArrayToBase64 from "../../Utils/uint8ArrayToBase64";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { IcpAccelerator_backend } from "../../../../../declarations/IcpAccelerator_backend/index";
+const investors = [
+  {
+    id: 1,
+    image: image,
+    name: "SamyKarim",
+    role: "Toshi, Managing Partner. Ex-Binance",
+    company: ["SRE", "Observability ", "Kubernetes"],
+  },
+  {
+    id: 2,
+    image: image,
+    name: "SamyKarim",
+    role: "Toshi, Managing Partner. Ex-Binance",
+    company: "Reliability Engineer and DevOps",
+  },
+  {
+    id: 2,
+    image: image,
+    name: "SamyKarim",
+    role: "Toshi, Managing Partner. Ex-Binance",
+    company: "Reliability Engineer and DevOps",
+  },
+  {
+    id: 2,
+    image: image,
+    name: "SamyKarim",
+    role: "Toshi, Managing Partner. Ex-Binance",
+    company: "Reliability Engineer and DevOps",
+  },
+  {
+    id: 2,
+    image: image,
+    name: "SamyKarim",
+    role: "Toshi, Managing Partner. Ex-Binance",
+    company: "Reliability Engineer and DevOps",
+  },
+];
+
+const ViewInvestor = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [noData, setNoData] = useState(null);
+
+  const actor = useSelector((currState) => currState.actors.actor);
+
+  const getAllInvestors = async (caller) => {
+    await caller
+      .list_all_vcs()
+      .then((result) => {
+        console.log("result-in-get-all-investors", result);
+        if (result?.length > 0) {
+          setNoData(false);
+          setData(result);
+        } else {
+          setNoData(true);
+          setData(investors);
+        }
+      })
+      .catch((error) => {
+        console.log("error-in-get-all-investors", error);
+      });
+  };
+
+  useEffect(() => {
+    if (actor) {
+      getAllInvestors(actor);
+    } else {
+      getAllInvestors(IcpAccelerator_backend);
+    }
+  }, [actor]);
+  return (
+    <div className="px-[4%] w-full bg-gray-100 h-screen overflow-y-scroll">
+    <div className="flex flex-col text-center items-center justify-center">
+        <div className="py-8">
+          <h2 className="text-[40px] font-black leading-10 bg-gradient-to-r from-[#7283EA] to-[#4087BF] bg-clip-text text-transparent transform">
+            Get{" "}
+            <span className=" bg-gradient-to-r from-[#3C04BA] to-[#4087BF] bg-clip-text text-transparent transform">
+              investments
+            </span>
+          </h2>
+        </div>
+        <div className="flex items-center relative md1:w-1/2 sm1:w-3/4 w-full p-2 mb-8 border border-[#737373] rounded-lg shadow-md">
+          <input
+            type="text"
+            placeholder="Search by company, skills or role"
+            className="flex-grow bg-transparent rounded focus:outline-none"
+          />
+          <button className="md1:block absolute hidden right-0 bg-[#3505B2] font-black text-xs text-white px-4 py-2 mr-1 rounded-md focus:outline-none">
+            Search Investor
+          </button>
+          <button className="block absolute md1:hidden right-0 bg-transparent font-black text-xs text-[#3505B2] px-4 py-2 mr-1 rounded-md focus:outline-none">
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    <div className="flex flex-wrap justify-center">
+      {data.map((investor, index) => {
+        let id = "";
+        let img = "";
+        let name = "";
+        let company = [];
+        let role = "";
+        if(noData){
+            id = investor.id;
+            img = investor.image;
+            name = investor.name;
+            company = investor.company;
+            role = investor.role;
+        }else{
+            id = investor[1]?.vc_profile?.uid;
+            img = uint8ArrayToBase64(
+              investor[1]?.vc_profile?.params?.user_data?.profile_picture
+            );
+            name = investor[1]?.vc_profile?.params?.user_data?.full_name;
+            company = investor[1]?.vc_profile?.params?.assets_under_management;
+            role = "Investor";
+        }
+
+        return (
+            <div className="">
+            <div className="w-full sm:w-full md:w-full lg:w-full xl:w-full p-4">
+                <div className="shadow-md rounded-lg overflow-hidden  drop-shadow-2xl gap-2 bg-[#C1CAFF]">
+                    <div className='flex flex-col sm:flex-row gap-6 p-2'>
+                        <img className='w-full sm:w-[300.53px] rounded-md h-auto sm:h-[200.45px] flex lg:items-center lg:justify-center  ' src={img} alt="alt" />
+                        <div className='flex flex-col w-full'>
+                            <h1 className="text-black text-2xl font-extrabold">SamyKarim</h1>
+                            <p className="text-[#737373]">Toshi, Managing Partner, Ex-Binance</p>
+                            <div className='flex flex-wrap bg-white rounded-full underline mt-6 text-[#737373] justify-center'>
+                                <p className=''>SRE</p>
+                                <p className=''>observability</p>
+                                <p className=''>Kubernetes</p>
+                            </div>
+
+                            <div className='w-100px border-2 text-gray-900 mt-2'></div>
+                            <div className='flex justify-end mt-6 xl:mr-8'>
+                                <button className=' font-bold py-2 px-4 bg-white text-[#7283EA] rounded-md'>
+                                    ReachOut
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+      })}
+    </div>
+    </div>
+  );
+};
+
+export default ViewInvestor;
