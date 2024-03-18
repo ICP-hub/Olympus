0798482fcfb19e205cf52411b2c302059940fbe4
aggarwalMohit30 <mohit.aggarwal@quadbtech.com>
2024-03-18 13:40:18 +0530

    Add Testimonial Modal

--- src/IcpAccelerator_frontend/src/components/Dashboard/RegisterCard.jsx
+++ src/IcpAccelerator_frontend/src/components/Dashboard/RegisterCard.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React,{useState} from "react";
+import { useNavigate } from "react-router-dom";
+import TestimonialModal from "../../models/TestimonialModal";
 
 const RegisterCard = ({ categories, redirect }) => {
   const navigate = useNavigate();
+  const [isTestimonialModalOpen, setTestimonialModalOpen] = useState(false);
+  const handleOpenModal = () => setTestimonialModalOpen(true);
+  const handleCloseModal = () => setTestimonialModalOpen(false);
 
   return (
     <div className="flex flex-wrap w-full">
@@ -15,23 +19,38 @@ const RegisterCard = ({ categories, redirect }) => {
                 src={category.imgSrc}
                 alt="not found"
               />
-              <div className='py-8'>
+              <div className="py-8">
                 <div className="text-xl sm:text-2xl text-white mt-4 font-bold line-clamp-2">
                   <h1>{category.title}</h1>
                 </div>
                 <p className="text-white mt-3 line-clamp-2">
                   {category.description}
                 </p>
-                <button onClick={() => navigate(`/${redirect}`)} className="mt-4 uppercase bg-[#7283EA] text-white px-4 py-2 rounded-xl w-full justify-center items-center font-extrabold">
-                  {category.buttonText}
-                </button>
+                {category.title === "Add your testimonial" ? (
+                  <button
+                    className="mt-4 uppercase bg-[#7283EA] text-white px-4 py-2 rounded-xl w-full justify-center items-center font-extrabold"
+                    onClick={handleOpenModal}
+                  >
+                    {category.buttonText}
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => navigate(`/${redirect}`)}
+                    className="mt-4 uppercase bg-[#7283EA] text-white px-4 py-2 rounded-xl w-full justify-center items-center font-extrabold"
+                  >
+                    {category.buttonText}
+                  </button>
+                )}
               </div>
             </div>
           </div>
         </div>
       ))}
+      {isTestimonialModalOpen && (
+        <TestimonialModal onClose={handleCloseModal} />
+      )}
     </div>
   );
-}
+};
 
-export default RegisterCard
+export default RegisterCard;
