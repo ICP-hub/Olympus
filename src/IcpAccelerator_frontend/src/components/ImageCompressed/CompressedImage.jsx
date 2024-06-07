8343a89b3a0413e00c4a23aa07e70e4deaf541f6
aggarwalMohit30 <mohit.aggarwal@quadbtech.com>
2024-06-08 02:11:42 +0530

    Remove bug from Money Raising and add resizer in image

--- src/IcpAccelerator_frontend/src/components/ImageCompressed/CompressedImage.jsx
+++ src/IcpAccelerator_frontend/src/components/ImageCompressed/CompressedImage.jsx
@@ -1,23 +1,47 @@
 import imageCompression from "browser-image-compression";
+import Resizer from "react-image-file-resizer";
 
 const compressImage = async (file) => {
   try {
+    // Resize the image
+    const resizedImage = await new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        300, // max width
+        300, // max height
+        "JPEG", // format
+        100, // quality
+        0, // rotation
+        (uri) => {
+          resolve(uri);
+        },
+        "blob" // output type
+      );
+    });
+
+    // Create a File from the resized image blob
+    const resizedFile = new File([resizedImage], file.name, {
+      type: file.type,
+    });
+
+    // Compress the resized image
     const options = {
-      maxSizeMB: 0.6, // Maximum file size you want after compression
-      maxWidthOrHeight: 1024, // Maximum size of the width or height of the image
+      maxSizeMB: 0.05, // Maximum file size you want after compression
+      maxWidthOrHeight: 300, // Maximum size of the width or height of the image
       useWebWorker: true, // Utilize Web Worker for performance improvement
     };
-    const compressedBlob = await imageCompression(file, options);
-    console.log('Compressed blob:', compressedBlob); // Debugging line, can be commented out
+    const compressedBlob = await imageCompression(resizedFile, options);
 
+    // Create a File from the compressed blob
     const compressedFile = new File([compressedBlob], file.name, {
       type: file.type,
     });
-    console.log("Compressed File:", compressedFile); // Debugging line, can be commented out
+
+    console.log("Resized and Compressed File:", compressedFile); // Debugging line, can be commented out
 
     return compressedFile;
   } catch (error) {
-    console.error("Error compressing image:", error);
+    console.error("Error resizing and compressing image:", error);
     throw error;
   }
 };
