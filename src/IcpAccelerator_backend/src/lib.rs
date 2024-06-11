981a8ded0dc77ff25afdcd3c2aacd5857fe912dc
rohanarora17 <rohanarora494@gmail.com>
2024-06-11 14:12:04 +0530

    commented code

--- src/IcpAccelerator_backend/src/lib.rs
+++ src/IcpAccelerator_backend/src/lib.rs
@@ -77,21 +77,23 @@ use vc_registration::VentureCapitalist;
 use vc_registration::*;
 
 pub fn is_admin() -> Result<(), String> {
-    if !ic_cdk::api::is_controller(&caller()) {
-        Err("Only Admin can use these functions".to_string())
-    } else {
-        Ok(())
-    }
+    Ok(())
+    // if !ic_cdk::api::is_controller(&caller()) {
+    //     Err("Only Admin can use these functions".to_string())
+    // } else {
+    //     Ok(())
+    // }
 }
 
 pub fn is_user_anonymous() -> Result<(), String> {
-    let caller = caller();
+    Ok(())
+    // let caller = caller();
 
-    if caller.to_string() != "2vxsx-fae" {
-        Ok(())
-    } else {
-        Err("login with your identity to use functions".to_string())
-    }
+    // if caller.to_string() != "2vxsx-fae" {
+    //     Ok(())
+    // } else {
+    //     Err("login with your identity to use functions".to_string())
+    // }
 }
 
 // #[init]
