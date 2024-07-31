3accf9fdfd8fb1f48cff4ee04a76727f517eb5c9
chandan-quadb <chandan.tech@quadbtech.com>
2024-07-31 15:15:58 +0530

    section2 update

--- src/IcpAccelerator_frontend/src/components/Home/Section2.jsx
+++ src/IcpAccelerator_frontend/src/components/Home/Section2.jsx
@@ -9,12 +9,23 @@ export default function Section2() {
   return (
    <>
      <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-white">
+     <div className="flex flex-col items-center justify-center  bg-white mx-auto pb-20 pt-28">
+      <div className="py-2 px-4 bg-white border border-blue-500 rounded-full text-blue-500 mb-4">
+        About
+      </div>
+      <h1 className="text-3xl font-bold text-center mb-4">
+        The <span className="">Web3</span> Ecosystems Launchpad
+      </h1>
+      <p className="text-center text-gray-600 max-w-3xl">
+        Est malesuada ac elit gravida vel aliquam nec. Arcu pellentesque convallis quam feugiat non viverra massa fringilla. Malesuada blandit integer quis tellus. Sit dolor lorem molestie a facilisis a integer laoreet tortor.
+      </p>
+    </div>
      <div className="container mx-auto">
      <div className="max-w-7xl w-full py-8 px-4 sm:px-6 lg:px-8  mx-auto">
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
         <div className="py-6  bg-[#E0F2FE] rounded-3xl overflow-hidden">
           <div className="flex items-center mb-4">
-            <img src={Founder} alt="Founders" className="w-[265px] h-[390px] mr-4" />
+            <img src={Founder} alt="Founders" className="w-[230px] h-[350px] mr-4" />
             <div className='px-3'>
               <h2 className="text-2xl font-bold">Founders</h2>
               <p className="text-smfont-normal text-[#4B5565] pb-6">
@@ -33,7 +44,7 @@ export default function Section2() {
         </div>
         <div className="py-6 bg-[#FEF0C7] rounded-3xl overflow-hidden">
           <div className="flex items-center mb-4">
-            <img src={Investor} alt="Investors" className="w-[265px] h-[390px] mr-4" />
+            <img src={Investor} alt="Investors" className="w-[230px] h-[350px] mr-4" />
             <div className='px-3'>
               <h2 className="text-2xl font-bold">Investors</h2>
               <p className="text-smfont-normal text-[#4B5565] pb-6">
@@ -52,7 +63,7 @@ export default function Section2() {
         </div >
         <div className="py-6 bg-[#FDEAD7] rounded-3xl overflow-hidden">
           <div className="flex items-center mb-4">
-            <img src={Investor} alt="Mentors" className="w-[265px] h-[390px] mr-4" />
+            <img src={Investor} alt="Mentors" className="w-[230px] h-[350px] mr-4" />
             <div className='px-3'> 
               <h2 className="text-2xl font-bold">Mentors</h2>
               <p className="text-smfont-normal text-[#4B5565] pb-6">
@@ -71,7 +82,7 @@ export default function Section2() {
         </div>
         <div className="py-6 bg-[#CCFBEF] rounded-3xl overflow-hidden">
           <div className="flex items-center mb-4">
-            <img src={Talents} alt="Talent" className="w-[265px] h-[390px] mr-4" />
+            <img src={Talents} alt="Talent" className="w-[230px] h-[350px] mr-4" />
             <div className='px-3'>
               <h2 className="text-2xl font-bold">Talent</h2>
               <p className="text-smfont-normal text-[#4B5565] pb-6">
@@ -89,7 +100,7 @@ export default function Section2() {
          
         </div>
       </div>
-      <div className="relative -top-[541px] flex items-center justify-center w-full mt-8">
+      <div className="relative -top-[501px] flex items-center justify-center w-full mt-8">
         <div className='bg-white p-6 rounded-full'>
         <button className="px-6 py-3 text-white bg-blue-600 rounded-full">
           Get started
