ba4f5331a8306538a1afb39649d1861872b075a1
aggarwalMohit30 <mohit.aggarwal@quadbtech.com>
2024-08-21 22:18:51 +0530

    accept reject code

--- src/IcpAccelerator_frontend/src/components/Dashboard/DashboardEvents/EventRequestCard.jsx
+++ src/IcpAccelerator_frontend/src/components/Dashboard/DashboardEvents/EventRequestCard.jsx
@@ -1,9 +1,25 @@
 // EventCard.js
-import React from "react";
+import React, { useState } from "react";
 import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
 import eventbg from "../../../../assets/images/bg.png";
 import ProfileImage from "../../../../assets/Logo/ProfileImage.png";
+import { FaFilter } from "react-icons/fa";
+
 const EventRequestCard = ({ event }) => {
+  const [isFilterOpen, setIsFilterOpen] = useState(false); // State to manage filter box visibility
+  const [selectedCategory, setSelectedCategory] = useState(""); // State to manage selected category
+  const [selectedType, setSelectedType] = useState(""); // State to manage selected type
+
+  const toggleFilter = () => {
+    setIsFilterOpen(!isFilterOpen); // Toggle the filter box
+  };
+
+  const handleApply = () => {
+    console.log("Selected Category:", selectedCategory);
+    console.log("Selected Type:", selectedType);
+    setIsFilterOpen(false); // Close the filter box after applying
+  };
+
   const events = [
     {
       date: "20 Jun – 22 Jun",
@@ -90,6 +106,100 @@ const EventRequestCard = ({ event }) => {
   ];
   return (
     <>
+      <div className='flex gap-3 items-center py-2 justify-between'><div className="flex items-center border-2 border-gray-400 rounded-lg overflow-hidden w-full h-[50px]">
+      <div className="flex items-center px-4">
+        <svg className="h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 103.5 3.5a7.5 7.5 0 0013.65 13.65z"></path>
+        </svg>
+      </div>
+      <input
+        type="text"
+        placeholder="Search people, projects, jobs, events"
+        className="w-full py-2 px-4 text-gray-700 focus:outline-none"
+      />
+      <div className="px-4">
+        <svg className="h-5 w-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 6h18M3 12h18m-7 6h7"></path>
+        </svg>
+      </div>
+    </div>
+    {/* <FaFilter className="text-gray-400 text-2xl"/> */}
+    <div>
+          <FaFilter 
+            className="text-gray-400 text-2xl cursor-pointer"
+            onClick={toggleFilter} // Toggle the filter box on click
+          />
+        </div>
+
+    </div>
+    {isFilterOpen && (
+            <div className="absolute  -mt-6 right-[6.9rem] w-52 bg-gray-100 border border-gray-300 p-4 rounded-lg shadow-lg z-50">
+            <div className="flex flex-col gap-4">
+              <h2 className="text-lg font-semibold text-gray-800">Filters</h2>
+              <div className="mb-1">
+                <span className="block text-sm font-medium text-gray-600 mb-2">Category</span>
+                <label className="flex items-center text-sm text-gray-700 font-medium mb-2">
+                  <input
+                    type="radio"
+                    name="category"
+                    value="Project"
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className="h-4 w-4 text-blue-600 form-radio checked:bg-blue-600 border border-black rounded-full cursor-pointer mr-2"
+                  />
+                  Project
+                </label>
+                <label className="flex items-center text-sm text-gray-700 font-medium mb-2">
+                  <input
+                    type="radio"
+                    name="category"
+                    value="Investor"
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className="h-4 w-4 text-blue-600 form-radio checked:bg-blue-600 border border-black rounded-full cursor-pointer mr-2"
+                  />
+                  Investor
+                </label>
+                <label className="flex items-center text-sm text-gray-700 font-medium">
+                  <input
+                    type="radio"
+                    name="category"
+                    value="Mentor"
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className="h-4 w-4 text-blue-600 form-radio checked:bg-blue-600 border border-black rounded-full cursor-pointer mr-2"
+                  />
+                  Mentor
+                </label>
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-600 mb-2">Types</label>
+                <select 
+                  onChange={(e) => setSelectedType(e.target.value)}
+                  className="block w-full bg-white border border-gray-300 rounded-md shadow-sm focus:border-gray-400 focus:ring  bg focus:ring-opacity-50 px-3 py-2">
+                  <option value="">All</option>
+                  <option value="Accepted">Accepted</option>
+                  <option value="Rejected">Rejected</option>
+                  <option value="Pending">Pending</option>
+                </select>
+              </div>
+              <div className="flex justify-between mt-1">
+                <button
+                  type="button"
+                  className="bg-gray-200 text-gray-700 px-4 py-1 rounded-md hover:bg-gray-300"
+                  onClick={toggleFilter} // Close the filter box on cancel
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white px-4 py-1 rounded-md hover:bg-blue-600"
+                  onClick={handleApply}
+                >
+                  Apply
+                </button>
+              </div>
+            </div>
+          </div>
+          
+      )}
     {events.map((event, index) => (
     <div className="bg-white rounded-lg shadow p-4 mb-6">
      
--- src/IcpAccelerator_frontend/src/components/Dashboard/DashboardEvents/FilterModal.jsx
+++ src/IcpAccelerator_frontend/src/components/Dashboard/DashboardEvents/FilterModal.jsx
@@ -34,70 +34,52 @@ const FilterModal = ({ isOpen, onClose }) => {
     ];
 
     return (
-        <div className={`fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ${isOpen ? 'block' : 'hidden'}`}>
-            <div className="bg-white rounded-lg p-6 w-[500px] shadow-lg">
+        
+            <div className={`fixed inset-0 z-50 ${isOpen ? 'block' : 'hidden'}`}>
+                <div className={`fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-2000 ease-out ${isOpen ? 'opacity-100 delay-1000' : 'opacity-0'}`} onClick={onClose}></div>
+                <div
+                    className={`fixed top-0 right-0 h-full bg-white w-[500px] p-6 shadow-lg transform transition-transform ease-in-out ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}
+                    style={{ transitionDuration: '2s' }}
+                >
+
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-lg font-semibold text-[#364152]">Filters</h2>
                     <button className="text-[#364152] text-xl" onClick={onClose}>&times;</button>
                 </div>
 
                 <div className="flex items-center gap-6 mb-4">
-                    <label
-                        className={`flex items-center justify-between px-2 py-2 border rounded-lg cursor-pointer hover:bg-gray-100 
-                        ${selectedCategory === 'Project' ? 'bg-gray-100' : ''}`}
-                        onClick={() => handleCategoryChange({ target: { value: 'Project' } })}
-                    >
-                        <div className="flex items-center">
-                            <span className="ml-4">
-                                <span className="font-semibold flex capitalize">Project</span>
-                            </span>
-                        </div>
+                    <label className="flex items-center text-sm text-[#364152] font-medium">
                         <input
                             type="radio"
                             name="category"
-                            className={`h-4 w-4 text-blue-600 border border-black rounded-full cursor-pointer 
-                            ${selectedCategory === 'Project' ? 'bg-blue-600' : ''}`}
+                            value="Project"
                             checked={selectedCategory === 'Project'}
                             onChange={handleCategoryChange}
+                            className="h-4 w-4 text-blue-600 form-radio checked:bg-blue-600 border border-black rounded-full cursor-pointer mr-2"
                         />
+                        Project
                     </label>
-                    <label
-                        className={`flex items-center justify-between px-2 py-2 border rounded-lg cursor-pointer hover:bg-gray-100 
-                        ${selectedCategory === 'Investor' ? 'bg-gray-100' : ''}`}
-                        onClick={() => handleCategoryChange({ target: { value: 'Investor' } })}
-                    >
-                        <div className="flex items-center">
-                            <span className="ml-4">
-                                <span className="font-semibold flex capitalize">Investor</span>
-                            </span>
-                        </div>
+                    <label className="flex items-center text-sm text-[#364152] font-medium">
                         <input
                             type="radio"
                             name="category"
-                            className={`h-4 w-4 text-blue-600 border border-black rounded-full cursor-pointer 
-                            ${selectedCategory === 'Investor' ? 'bg-blue-600' : ''}`}
+                            value="Investor"
                             checked={selectedCategory === 'Investor'}
                             onChange={handleCategoryChange}
+                            className="h-4 w-4 text-blue-600 form-radio checked:bg-blue-600 border border-black rounded-full cursor-pointer mr-2"
                         />
+                        Investor
                     </label>
-                    <label
-                        className={`flex items-center justify-between px-2 py-2 border rounded-lg cursor-pointer hover:bg-gray-100 
-                        ${selectedCategory === 'Mentor' ? 'bg-gray-100' : ''}`}
-                        onClick={() => handleCategoryChange({ target: { value: 'Mentor' } })}
-                    >
-                        <div className="flex items-center">
-                            <span className="ml-4">
-                                <span className="font-semibold flex capitalize">Mentor</span>
-                            </span>
-                        </div>
+                    <label className="flex items-center text-sm text-[#364152] font-medium">
                         <input
                             type="radio"
                             name="category"
-                            className={`h-4 w-4 text-blue-600 border border-black rounded-full cursor-pointer 
-                            ${selectedCategory === 'Mentor' ? 'bg-blue-600' : ''}`}
+                            value="Mentor"
                             checked={selectedCategory === 'Mentor'}
                             onChange={handleCategoryChange}
+                            className="h-4 w-4 text-blue-600 form-radio checked:bg-blue-600 border border-black rounded-full cursor-pointer mr-2"
                         />
+                        Mentor
                     </label>
                 </div>
 
@@ -106,7 +88,7 @@ const FilterModal = ({ isOpen, onClose }) => {
                     <select
                         value={selectedType}
                         onChange={handleTypeChange}
-                        className="block w-full mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
+                        className="block w-full mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 pl-3"
                     >
                         <option value="">All</option>
                         {ServiceOptions.map(option => (
