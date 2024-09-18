0e5c49cca5066da89736c2b07aa0f00c9943c933
chandan-qauadb <chandan.tech@quadbtech.com>
2024-09-18 16:06:44 +0530

    fix responsiveness issue in discover section

--- src/IcpAccelerator_frontend/src/components/Discover/ProjectAlldata.jsx
+++ src/IcpAccelerator_frontend/src/components/Discover/ProjectAlldata.jsx
@@ -272,13 +272,13 @@ const DiscoverProject = ({ onProjectCountChange }) => {
             const principle_id = projectArray[0];
             return (
               <div
-                className="p-6 w-[750px] rounded-lg shadow-sm mb-4 flex"
+                className="sm:p-6 my-10 sm:my-0 w-full sm:w-[750px] rounded-lg shadow-sm mb-4 flex flex-col sm:flex-row"
                 key={index}
               >
-                <div className="w-[272px] relative">
+                <div className="w-full sm:w-[272px] relative">
                   <div
                     onClick={() => handleClick(principle_id, user)}
-                    className="max-w-[250px] w-[250px] h-[254px] bg-gray-100 rounded-lg flex flex-col justify-between relative overflow-hidden"
+                    className="w-full sm:max-w-[250px] sm:w-[250px] h-[254px] bg-gray-100 rounded-lg flex flex-col justify-between relative overflow-hidden"
                   >
                     <div className="absolute inset-0 flex items-center justify-center">
                       <img
@@ -297,7 +297,7 @@ const DiscoverProject = ({ onProjectCountChange }) => {
                   </div>
                 </div>
 
-                <div className="flex-grow ml-[25px] w-[544px]">
+                <div className="flex-grow mt-5 sm:mt-0 sm:ml-[25px] w-full sm:w-[544px]">
                   <div className="flex justify-between items-start mb-2">
                     <div>
                       <div>
