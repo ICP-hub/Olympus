aef3fd77ad77741e591fe9febf057fa48cd54dc1
MridulQB <mridul.yadav@quadbtech.com>
2024-10-31 23:36:08 +0530

    feat(BackEnd): Fixed Notification System And Added User Data

--- src/IcpAccelerator_backend/src/notifications/notification.rs
+++ src/IcpAccelerator_backend/src/notifications/notification.rs
@@ -1,7 +1,7 @@
 use candid::{CandidType, Principal};
 use serde::{Deserialize, Serialize};
 
-use crate::{state_handler::{mutate_state, read_state, Candid, StoredPrincipal}, AccessRequest, CohortEnrollmentRequest, MentorInternal, ProjectInfoInternal, UserInfoInternal, VentureCapitalist};
+use crate::{state_handler::{mutate_state, read_state, Candid, StoredPrincipal}, user_modules::get_user::get_user_info_using_principal, AccessRequest, CohortEnrollmentRequest, MentorInternal, ProjectInfoInternal, UserInfoInternal, VentureCapitalist};
 
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
 pub struct AssociationNotificationProjectToInvestor{
@@ -54,17 +54,23 @@ pub struct NotificationInternal{
 
 #[derive(CandidType, Serialize, Deserialize, Clone, Debug)]
 pub struct NotificationStructure{
-    pub sender: Principal,
-    pub receiver: Principal,
+    pub sender_principal: Principal,
+    pub receiver_principal: Principal,
+    pub sender_data: UserInfoInternal,
+    pub receiver_data: UserInfoInternal,
     pub notification_data: NotificationInternal,
     pub sent_at: u64,
 }
 
 #[ic_cdk::update]
 pub fn add_notification(sender_principal: Principal, receiver_principal: Principal, noti_data: NotificationInternal,) -> Result<String, String> {
+    let sender_user_data = get_user_info_using_principal(sender_principal).unwrap();
+    let receiver_user_data = get_user_info_using_principal(receiver_principal).unwrap();
     let notification = NotificationStructure {
-        sender: sender_principal,
-        receiver: receiver_principal,
+        sender_principal: sender_principal,
+        receiver_principal: receiver_principal,
+        sender_data: sender_user_data,
+        receiver_data: receiver_user_data,
         notification_data: noti_data,
         sent_at: ic_cdk::api::time(),
     };
